b1feba4ec84d34bc48734665144c44fa996fa0af
Michal Vrbata <vrbata@outlook.com>
2025-05-20 19:51:51 +0200

    Update dependencies

--- app/middleware/locale-middleware.ts
+++ app/middleware/locale-middleware.ts
@@ -5,15 +5,17 @@ const localeMiddleware: Route.unstable_MiddlewareFunction = async (
   { request },
   next,
 ) => {
-  const response = await paraglideMiddleware(request, () => {
-    return next();
-  });
-
-  if (response.status === 307) {
-    throw response;
-  }
-
-  return response;
+  return await paraglideMiddleware(
+    request,
+    () => {
+      return next();
+    },
+    {
+      onRedirect: (response) => {
+        throw response;
+      },
+    },
+  );
 };
 
 export { localeMiddleware };
